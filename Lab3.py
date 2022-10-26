0fd7c9e637508f3b62dba8d0f53d75b6c86bdb3c
Nathanael Ostheller <nathanael.ostheller@wsu.edu>
2022-10-25 17:58:00 -0700

    Finished problems 1 and 2

--- Lab3.py
+++ Lab3.py
@@ -1,14 +1,34 @@
 # CptS 355 - Fall 2022 - Lab 3
-# Please include your name here
+# Nathanael Ostheller
+
+from calendar import month
+from queue import Empty
+
 
 debugging = False
 def debug(*s): 
      if debugging: 
           print(*s)
 
-## problem 1 getNumCases 
+## problem 1 getNumCases
+def getNumCases(data, counties, months):
+     casesSum = 0
+     for county in counties:
+          if (data[county]):
+               for month in months:
+                    if((data[county])[month]):
+                         casesSum = casesSum + (data[county])[month]
+     return casesSum
 
 ## problem 2 getMonthlyCases
+def getMonthlyCases(data):
+     monthlyDict = {}
+     for county,log in data.items():
+          for month,count in log.items():
+               if month not in monthlyDict.keys():
+                    monthlyDict[month] = {}
+               monthlyDict[month][county] = count
+     return monthlyDict
 
 from functools import reduce
 ## problem 3 mostCases 
